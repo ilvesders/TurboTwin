6e38d20aef180ef832574c2d63d3b1a4e9cb5704
ilvesders <nothingmightbeok@mail.ru>
2024-02-12 00:11:10 +0400

    finishedcononoutsideclickclose

--- scripts/app.js
+++ scripts/app.js
@@ -10,17 +10,26 @@ function showInfo(info) {
     event.currentTarget.classList.add('active');
 
     infoContainer.style.display = 'block';
-}
+};
 
 document.getElementById("open-model-btn").addEventListener("click",function(){
     document.getElementById("my-model").classList.add("open")
-})
+});
 
 document.getElementById("close-my-model-btn").addEventListener("click",function(){
     document.getElementById("my-model").classList.remove("open")
-})
+});
 window.addEventListener('keydown', (e) => {
     if (e.key === "Escape") {
         document.getElementById("my-model").classList.remove("open")
     }
-})
+});
+
+document.querySelector("#my-model .modal__box").addEventListener("click", event => {
+    event._isClickWithInModal = true;
+});
+
+document.getElementById("my-model").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
